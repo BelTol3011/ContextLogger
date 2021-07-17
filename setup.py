f927cccf057495337ca9d8bdbedc1fb87ab234bb
Bela <bela.tollking@gmail.com>
2021-07-17 10:53:48 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='context_logger',
+    version='1.0',
+    packages=['context_logger'],
+    url='',
+    license='',
+    author='Belissimo',
+    author_email='bela.tollking@gmail.com',
+    description='A logger library that is based on the current context.'
+)
