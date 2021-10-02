f5f5f8d7038e8229b33b9e2ae98647a4cece7d74
bela <bela.tollking@gmail.com>
2021-10-02 12:22:01 +0200

    reformat setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
-setup(
-    name='context_logger',
-    version='1.0',
-    packages=['context_logger'],
-    url='',
-    license='',
-    author='Belissimo',
-    author_email='bela.tollking@gmail.com',
-    description='A logger library that is based on the current context.'
-)
+
+if __name__ == "__main__":
+    setup(
+        name='context_logger',
+        version='1.0',
+        packages=['context_logger'],
+        url='',
+        license='',
+        author='Belissimo',
+        author_email='bela.tollking@gmail.com',
+        description='A logger library that is based on the current context.'
+    )
